6a94e66cc27fb802e76d96034b0fbce150bfca6f
SyominAlex <gurufx.43@gmail.com>
2021-12-19 11:49:16 +0300

    ДЗ к уроку 4

---
+++ index.js
@@ -0,0 +1,9 @@
+//const lesson1 = require("./01");
+//const lesson2 = require("./02");
+// const lesson3 = require("./03");
+const lesson4 = require("./04");
+
+//lesson1();
+//lesson2();
+// lesson3();
+lesson4();
